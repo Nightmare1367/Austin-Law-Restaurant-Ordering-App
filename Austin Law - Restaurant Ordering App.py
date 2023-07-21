8086da0377980485a63ada5773c9d57176538bfb
Austin Law <demon13267@gmail.com>
2023-07-21 13:15:12 +1200

    Update Austin Law - Restaurant Ordering App.py
    Changes to Image and Font for text

--- Austin Law - Restaurant Ordering App.py
+++ Austin Law - Restaurant Ordering App.py
@@ -16,21 +16,22 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 class TopNavBar(customtkinter.CTkFrame):
+    global indicate, dessert_indicator, hide_indicate
     def __init__(self, master):
         super().__init__(master)
         # ---------------------------------- Widgets ---------------------------------- #
         # region | Buttons for the Top Navigation Bar
         main_btn = customtkinter.CTkButton(self, text="Mains", 
-                                           font=customtkinter.CTkFont(family="Verdana", size=30), 
+                                           font=customtkinter.CTkFont(family="Calibri", size=35), 
                                            fg_color="transparent", text_color="black", hover_color="gray",
                                            command=lambda: indicate(main_indicator))
         main_btn.grid(column=0, row=0, padx=10, pady=10, sticky='we')
 
-        appetisers_btn = customtkinter.CTkButton(self, text="Appetisers", font=customtkinter.CTkFont(family="Verdana", size=30), 
+        appetisers_btn = customtkinter.CTkButton(self, text="Appetisers", font=customtkinter.CTkFont(family="Calibri", size=35), 
                                            fg_color="transparent",text_color="black", hover_color="gray", command=lambda: indicate(appetisers_indicator))
         appetisers_btn.grid(column=1, row=0, padx=10, pady=10, sticky='we')
 
-        dessert_btn = customtkinter.CTkButton(self, text="Desserts", font=customtkinter.CTkFont(family="Verdana", size=30), 
+        dessert_btn = customtkinter.CTkButton(self, text="Desserts", font=customtkinter.CTkFont(family="Calibri", size=35), 
                                            fg_color="transparent",text_color="black", hover_color="gray", command=lambda: indicate(dessert_indicator))
         dessert_btn.grid(column=2, row=0, padx=10, pady=10, sticky='we')
         # endregion
@@ -61,11 +62,10 @@ class TopNavBar(customtkinter.CTkFrame):
 class MainsSelection(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
-
         # region | Label for the Mains Page
         title = customtkinter.CTkLabel(self, text="Mains", 
                                        font=customtkinter.CTkFont(size=50, weight="normal", 
-                                                                  underline=True, family="Verdana"), text_color="white")
+                                                                  underline=True, family="Calibri"), text_color="white")
         title.grid(column=0, row=0, pady= 10, padx=35, sticky="nw")
         # endregion
 
@@ -73,60 +73,74 @@ class MainsSelection(customtkinter.CTkFrame):
         # ---------------------------------- Frames ---------------------------------- #
         dfl = 6               # Number of display frames
         # List to store widgets 
-        display_frames_list = []
+        main_frames_list = []
         # Loop that runs depending on the dfl value divided by the desired number of columns
         for make_list in range((round(dfl/3))):
-            display_frames_list.append([])        # Appends empty list into the main list 
+            main_frames_list.append([])        # Appends empty list into the main list 
 
 
         # Loop that runs for for however long the dfl value is
-        for display_frame in range(dfl):
+        for main_frame in range(dfl):
             self.rowitem = customtkinter.CTkFrame(self, corner_radius=10, width=300)        # Creates a base frame for the item display
 
             # ---------- If Conditions ---------- #
             # Conditioned to only run when the display frame value is less than 3
-            if display_frame < 3:
-                self.rowitem.grid(column=0 + display_frame, row=1, sticky="nsew", pady=20, padx=15)     # Grids the frame to row 1 and increases by 1 in columns
-                display_frames_list[0].append(self.rowitem)
+            if main_frame < 3:
+                self.rowitem.grid(column=0 + main_frame, row=1, sticky="nsew", pady=20, padx=15)     # Grids the frame to row 1 and increases by 1 in columns
+                main_frames_list[0].append(self.rowitem)
             # Conditioned to run when the display frame value is more or equal to 3
-            if display_frame >= 3:
-                self.rowitem.grid(column=0 + (display_frame - 3), row=2, sticky="nsew", pady=20, padx=15)   # Grids the frame to row 2 and would subtract 3 to reset the column position
-                display_frames_list[1].append(self.rowitem)
+            if main_frame >= 3:
+                self.rowitem.grid(column=0 + (main_frame - 3), row=2, sticky="nsew", pady=20, padx=15)   # Grids the frame to row 2 and would subtract 3 to reset the column position
+                main_frames_list[1].append(self.rowitem)
         
         # ---------------------------------- Images ---------------------------------- #
         # Cutlet Image
-        cutlet = customtkinter.CTkImage(Image.open("Images/Cutlet.jpg"), size=(275, 200))
-        cutlet_img = customtkinter.CTkLabel(display_frames_list[0][0], image=cutlet, text="")
-        cutlet_img.grid(row=0, column=0, sticky="news", pady=20, padx=20)
+        cutlet = customtkinter.CTkImage(Image.open("Images/Cutlet.jpg"), size=(275, 200))               # Imports image 
+        cutlet_imgbtn = customtkinter.CTkButton(main_frames_list[0][0], image=cutlet, text="",       # Puts the image in a button
+                                                fg_color='transparent', hover_color="#333333")          # Font Configuration
+        cutlet_imgbtn.grid(row=0, column=0, sticky="news", pady=(15,10), padx=10)                       # Gridding for the image button
+
+        """cutlet_img = customtkinter.CTkLabel(display_frames_list[0][0], image=cutlet, text="")
+        cutlet_img.grid(row=0, column=0, sticky="news", pady=20, padx=20)"""
 
         # Lasagna Image
-        lasagna = customtkinter.CTkImage(Image.open("Images/Lasagna.jpg"), size=(275, 200))
-        lasagna_img = customtkinter.CTkLabel(display_frames_list[0][1], image=lasagna, text="")
-        lasagna_img.grid(row=0, column=0, sticky="news", pady=20, padx=20)
+        lasagna = customtkinter.CTkImage(Image.open("Images/Lasagna.jpg"), size=(275, 200))             # Imports image 
+        lasagna_imgbtn = customtkinter.CTkButton(main_frames_list[0][1], image=lasagna, text="",     # Puts the image in a button
+                                                 fg_color='transparent', hover_color="#333333")         # Font Configuration
+        lasagna_imgbtn.grid(row=0, column=0, sticky="news", pady=(15,10), padx=10)                      # Gridding the image button
+
+        """lasagna_img = customtkinter.CTkLabel(display_frames_list[0][1], image=lasagna, text="")
+        lasagna_img.grid(row=0, column=0, sticky="news", pady=20, padx=20)"""
 
         # Burger Image
-        burger = customtkinter.CTkImage(Image.open("Images/Burger.jpg"), size=(275, 200))
+        burger = customtkinter.CTkImage(Image.open("Images/Burger.jpg"), size=(275, 200))               # Imports image 
+        burger_imgbtn = customtkinter.CTkButton(main_frames_list[0][2], image=burger, text="",       # Puts the image in a button
+                                                fg_color='transparent', hover_color="#333333")          # Font Configuration
+        burger_imgbtn.grid(row=0, column=0, sticky="news", pady=(15,10), padx=10)                       # Gridding the image button
+
         """burger_img = customtkinter.CTkLabel(display_frames_list[0][2], image=burger, text="")
         burger_img.grid(row=0, column=0, sticky="news", pady=20, padx=20)"""
 
         # ----------------------------- Buttons / Labels ----------------------------- #
         # Cutlet Button / Label
-        cutlet_lbl = customtkinter.CTkButton(display_frames_list[0][0], text="Chicken Cutlet \n Price:", 
-                                            font=customtkinter.CTkFont(family='Verdana', weight='bold', size=20))
+        cutlet_lbl = customtkinter.CTkButton(main_frames_list[0][0], text="Chicken Cutlet \n Price: $21.99", 
+                                            font=customtkinter.CTkFont(family='Calibri', weight='bold', size=25))
         cutlet_lbl.grid(row=1, column=0, sticky='new', pady=(5,10), padx=10)
 
         # Lasagna Button / Label
-        lasagna_lbl = customtkinter.CTkButton(display_frames_list[0][1], text="Lasagna", 
-                                             font=customtkinter.CTkFont(family='Verdana', weight='bold', size=20))
+        lasagna_lbl = customtkinter.CTkButton(main_frames_list[0][1], text="Lasagna \n Price: $15.00", 
+                                             font=customtkinter.CTkFont(family='Calibri', weight='bold', size=25))
         lasagna_lbl.grid(row=1, column=0, sticky='new', pady=(5,10), padx=10)
 
         # Burger Button / Label
-        burger_lbl = customtkinter.CTkButton(display_frames_list[0][2], text="Beef Burger", 
-                                             font=customtkinter.CTkFont(family='Verdana', weight='bold', size=20))
+        burger_lbl = customtkinter.CTkButton(main_frames_list[0][2], text="Beef Burger \n Price: $13.00", 
+                                             font=customtkinter.CTkFont(family='Calibri', weight='bold', size=25))
         burger_lbl.grid(row=1, column=0, sticky='new', pady=(5,10), padx=10)
 
-        burger_imgbtn = customtkinter.CTkButton(display_frames_list[0][2], image=burger, text="")
-        burger_imgbtn.grid(row=0, column=0, sticky="news", ipady=20, ipadx=10)
+        # Testing a add to order Button
+        ato_btn = customtkinter.CTkButton(main_frames_list[0][0], text = "+", width=40,
+                                          font=customtkinter.CTkFont(family='Calibri', size=20, weight='bold'))
+        ato_btn.grid(row=0, column=0, sticky='ne', pady=25, padx=25)
 
 
 class App(customtkinter.CTk):
