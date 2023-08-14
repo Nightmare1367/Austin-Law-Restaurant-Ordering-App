65a77478be942b0c7ea81e5c8830577af74cceaa
Austin Law <demon13267@gmail.com>
2023-08-14 13:10:56 +1200

    Create about_page.py
    Description for the items

---
+++ about_page.py
@@ -0,0 +1,77 @@
+description = {
+        # Mains
+        "Chicken Cutlet"        : ["A japanese dish which contains fried chicken made with panko crumbs, "
+                                   "topped with an egg and a salad and rice on the side", 
+                                   "Flour, Panko Breadcrumbs, Chicken Breast, Eggs, Rice and an Assortment of Salad"],
+
+        "Lasagna"               : ["Lasagna is a pasta dish which originates from italy and this dish"
+                                   " is usually made with sheets of pasta with a special filling in between"
+                                   " and cheese on the top", 
+                                   "Tomato Sauce, Cheese, Pasta, Meat, Onion, Garlic, Egg, Spices"],
+
+        "Beef Burger"           : ["", ""],
+
+        "Butter Chicken"        : ["", ""],
+
+        "Pasta"                 : ["", ""],
+
+        "Pizza"                 : ["", ""],
+
+        "Mains Placeholder"     : ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
+                                   "eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit"],
+
+
+        # Appetiser
+        "Shoe String Fries"     : ["", ""],
+
+        "Hand Cut Fries"        : ["", ""],
+
+        "Onion Rings"           : ["", ""],
+
+        "Caesar Salad"          : ["", ""],
+
+        "Chicken Nibbles"       : ["", ""],
+
+        "Garlic Bread"          : ["", ""],
+
+        "Seafood Chowder"       : ["", ""],
+
+        "Appetiser Placeholder" : ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
+                                   "eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit"],
+
+
+        # Desserts
+        "Apple Crumble"         : ["", ""],
+
+        "Brownie"               : ["", ""],
+
+        "Cheesecake"            : ["", ""],
+
+        "Ice Cream"             : ["", ""],
+
+        "Macaron"               : ["", ""],
+
+        "Panna Cotta"           : ["", ""],
+
+        "Dessert Placeholder"   : ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
+                                   "eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit"],
+
+
+        # Drinks
+        "Coca-Cola"             : ["", ""],
+
+        "Chocolate Milkshake"   : ["", ""],
+
+        "Strawberry Juice"      : ["", ""],
+
+        "Bubble Tea"            : ["", ""],
+
+        "Hot Chocolate"         : ["", ""],
+
+        "Coffee"                : ["", ""],
+
+}
+
